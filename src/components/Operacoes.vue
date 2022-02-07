e30a3887ba2ae15635645e46964dfe2ad64e7bb6
Julie Caron <julie.faccenda@gmail.com>
2022-02-07 13:31:28 -0300

    finalização das funcionalidades

--- src/components/Operacoes.vue
+++ src/components/Operacoes.vue
@@ -2,31 +2,30 @@
   <div class="row justify-content-md-center">
     <div class="col-3">
       <div class="calculadora">
-        <div class="display form-control">{{ valorAtual || "0" }}</div>
-        <div @click="limparCalculadora" class="botao btn btn-info">C</div>
-        <div @click="atribuiValor" class="botao btn btn-info">+/-</div>
-        <div @click="atribuiValor" class="botao btn btn-info">%</div>
-        <div @click="atribuiValor" class="botao btn btn-info">%</div>
+        <div class="display form-control">{{ atualValue || "0" }}</div>
+        <div @click="cleanCalculator" class="botao btn btn-info">C</div>
+        <div @click="changeSignal" class="botao btn btn-info">+/-</div>
+        <div @click="percentValue" class="botao btn btn-info">%</div>
+        <div @click="division" class="botao operadores btn btn-info">÷</div>
         <div class="w-100"></div>
-        <div @click="atribuiValor('7')" class="botao btn btn-light">7</div>
-        <div @click="atribuiValor('8')" class="botao btn btn-light">8</div>
-        <div @click="atribuiValor('9')" class="botao btn btn-light">9</div>
-        <div class="botao operadores btn btn-info">÷</div>
+        <div @click="setValue('7')" class="botao btn btn-light">7</div>
+        <div @click="setValue('8')" class="botao btn btn-light">8</div>
+        <div @click="setValue('9')" class="botao btn btn-light">9</div>
+        <div @click="multiply" class="botao operadores btn btn-info">x</div>
         <div class="w-100"></div>
-        <div @click="atribuiValor('4')" class="botao btn btn-light">4</div>
-        <div @click="atribuiValor('5')" class="botao btn btn-light">5</div>
-        <div @click="atribuiValor('6')" class="botao btn btn-light">6</div>
-        <div class="botao operadores btn btn-info">x</div>
+        <div @click="setValue('4')" class="botao btn btn-light">4</div>
+        <div @click="setValue('5')" class="botao btn btn-light">5</div>
+        <div @click="setValue('6')" class="botao btn btn-light">6</div>
+        <div @click="dim" class="botao operadores btn btn-info">-</div>
         <div class="w-100"></div>
-        <div @click="atribuiValor('1')" class="botao btn btn-light">1</div>
-        <div @click="atribuiValor('2')" class="botao btn btn-light">2</div>
-        <div @click="atribuiValor('3')" class="botao btn btn-light">3</div>
-        <div class="botao operadores btn btn-info">-</div>
+        <div @click="setValue('1')" class="botao btn btn-light">1</div>
+        <div @click="setValue('2')" class="botao btn btn-light">2</div>
+        <div @click="setValue('3')" class="botao btn btn-light">3</div>
+        <div @click="sum" class="botao operadores btn btn-info">+</div>
         <div class="w-100"></div>
-        <div @click="atribuiValor" class="botao btn btn-info">.</div>
-        <div @click="atribuiValor('0')" class="botao btn btn-light">0</div>
-        <div class="botao btn btn-info">=</div>
-        <div class="botao operadores btn btn-info">+</div>
+        <div class="botao btn btn-info">.</div>
+        <div @click="setValue('0')" class="botao btn btn-light">0</div>
+        <div @click="result" class="botao btn btn-info">=</div>
       </div>
     </div>
   </div>
@@ -36,16 +35,62 @@
 export default {
   data() {
     return {
-      valorAtual: "",
+      atualValue: "",
+      lastValue: null,
+      operationType: null,
+      operationSelect: false,
     };
   },
   methods: {
     //   atribui o valor vazio para o valor atual
-    limparCalculadora() {
-      this.valorAtual = "";
+    cleanCalculator() {
+      this.atualValue = "";
     },
-    atribuiValor(n) {
-      this.valorAtual = this.valorAtual + n;
+    setValue(n) {
+      // se clicar em algum operador, retorna valor vazio e false
+      if (this.operationSelect) {
+        this.atualValue = "";
+        this.operationSelect = false;
+      }
+      this.atualValue = `${this.atualValue}${n}`;
+    },
+    changeSignal() {
+      //O método charAt() retorna o caractere especificado a partir de uma string. -
+      // O método slice() retorna uma cópia de parte de um array a partir de um subarray criado
+      this.atualValue =
+        this.atualValue.charAt(0) === "-"
+          ? this.atualValue.slice(1)
+          : `-${this.atualValue}`;
+    },
+    percentValue() {
+      this.atualValue = `${parseFloat(this.atualValue) / 100}`;
+    },
+    setLastValue() {
+      this.lastValue = this.atualValue;
+      this.operationSelect = true;
+    },
+    division() {
+      this.operationType = (num1, num2) => num1 / num2;
+      this.setLastValue();
+    },
+    sum() {
+      this.operationType = (num1, num2) => num1 + num2;
+      this.setLastValue();
+    },
+    multiply() {
+      this.operationType = (num1, num2) => num1 * num2;
+      this.setLastValue();
+    },
+    dim() {
+      this.operationType = (num1, num2) => num1 - num2;
+      this.setLastValue();
+    },
+    result() {
+      this.atualValue = `${this.operationType(
+        parseFloat(this.lastValue),
+        parseFloat(this.atualValue)
+      )}`;
+      this.lastValue = null;
     },
   },
 };
